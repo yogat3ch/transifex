12431a42cea579d9111b43bc3e760fc330a509f6
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2023-12-01 17:16:33 -0500

    `list_languages`: retrieving languages from the List Languages endpoint

---
+++ R/languages.R
@@ -0,0 +1,12 @@
+list_languages <- function(filter_code = "es", filter_code_any = NULL) {
+  query <- purrr::compact(list(
+    `filter[code]` = filter_code,
+    `filter[code][any]` = filter_code_any
+  ))
+
+  req <- req_init() |>
+    httr2::req_url_path("languages") |>
+    httr2::req_url_query(!!!query)
+
+  req_do(req)
+}
