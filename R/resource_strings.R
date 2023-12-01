7dc46b82120cad9ed4d1488e4746153805456a95
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2023-12-01 17:17:24 -0500

    `get_resource_translations_collection`

--- R/resource_strings.R
+++ R/resource_strings.R
@@ -1,6 +1,6 @@
-get_resource_strings_collection <- function(resource = "o:virga-labs:p:dmdu:r:local_dev", filter_gte = NULL, filter_lte = NULL, filter_key = NULL, tag_all = NULL, tag_any = NULL, tag_query = NULL, limit = 150L, page = NULL) {
+get_resource_strings_collection <- function(resource = filter_string(organization = "virga-labs", project = "dmdu", resource = "local_dev"), filter_gte = NULL, filter_lte = NULL, filter_key = NULL, tag_all = NULL, tag_any = NULL, tag_query = NULL, limit = 150L, page = NULL) {
   query <- purrr::compact(list(
-    `filter[resource]` = prefix_o(resource),
+    `filter[resource]` = resource,
     `filter[strings_data_modified][gte]` = filter_gte,
     `filter[strings_data_modified][lte]` = filter_lte,
     `filter[key]` = filter_key,
@@ -16,5 +16,27 @@ get_resource_strings_collection <- function(resource = "o:virga-labs:p:dmdu:r:lo
     httr2::req_url_query(!!!query)
 
   resp <- req_do(req)
+
+}
+
+get_resource_translations_collection <- function(resource = filter_string(organization = "virga-labs", project = "dmdu", resource = "local_dev"), language = filter_string(language = "es"), filter_gt = NULL, filter_lt = NULL, filter_key = NULL, tag_all = NULL, tag_any = "rob_vul_metric", tag_query = NULL, limit = 150L, page = NULL) {
+  query <- purrr::compact(list(
+    `filter[resource]` = resource,
+    `filter[language]` = language,
+    `filter[date_translated][gt]` = filter_gt,
+    `filter[date_translated][gt]` = filter_lt,
+    `filter[resource_string][key]` = filter_key,
+    `filter[resource_string][tags][any]` = tag_any,
+    `filter[resource_string][tags][all]` = tag_all,
+    `filter[resource_string][tags][query]` = tag_query,
+    limit = limit,
+    `page[cursor]` = page
+  ))
+
+  req <- req_init() |>
+    httr2::req_url_path("resource_translations") |>
+    httr2::req_url_query(!!!query)
+
+  req_do(req)
 
 }
